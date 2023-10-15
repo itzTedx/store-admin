3ad47853ac2c3311cd3f2e71e13edb2e96920202
melwin-tedd <musictrendiez@gmail.com>
2023-10-16 02:19:50 +0400

    Category API

--- app/(dashboard)/[storeId]/(routes)/categories/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/components/cell-action.tsx
@@ -7,7 +7,7 @@ import { CopyCheck, Edit, MoreHorizontal, Trash2 } from 'lucide-react'
 import axios from 'axios'
 
 import { Button } from '@/components/ui/button'
-import { BillboardColumn } from './columns'
+import { CategoryColumn } from './columns'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -19,7 +19,7 @@ import {
 import { AlertModal } from '@/components/modals/alert-modal'
 
 interface CellActionProps {
-  data: BillboardColumn
+  data: CategoryColumn
 }
 
 const CellAction: React.FC<CellActionProps> = ({ data }) => {
@@ -37,12 +37,12 @@ const CellAction: React.FC<CellActionProps> = ({ data }) => {
   const onDelete = async () => {
     try {
       setLoading(true)
-      await axios.delete(`/api/${params.storeId}/billboards/${data.id}`)
+      await axios.delete(`/api/${params.storeId}/categories/${data.id}`)
       router.refresh()
-      toast.success('Billboard deleted.')
+      toast.success('Category deleted.')
     } catch (error: any) {
       toast.error(
-        'Make sure you removed all categories using this billboard first.'
+        'Make sure you removed all products using this category first.'
       )
     } finally {
       setLoading(false)
@@ -73,13 +73,17 @@ const CellAction: React.FC<CellActionProps> = ({ data }) => {
           </DropdownMenuItem>
           <DropdownMenuItem
             onClick={() =>
-              router.push(`/${params.storeId}/billboards/${data.id}`)
+              router.push(`/${params.storeId}/categories/${data.id}`)
             }
           >
             <Edit className='w-4 h-4 mr-2' />
             Edit
           </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setOpen(true)}>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            onClick={() => setOpen(true)}
+            className='hover:bg-red-500 hover:text-white transition-colors'
+          >
             <Trash2 className='w-4 h-4 mr-2' />
             Delete
           </DropdownMenuItem>
