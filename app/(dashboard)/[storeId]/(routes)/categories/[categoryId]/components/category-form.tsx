3f8fbf16eeddcf04d106c575a7ff9ddc5439770e
itzTedx <zomboi.offi@gmail.com>
2023-10-18 18:49:58 +0400

    Category Bug Fix

--- app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/components/category-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/components/category-form.tsx
@@ -1,17 +1,17 @@
-'use client'
+"use client";
 
-import * as z from 'zod'
-import axios from 'axios'
-import { useState } from 'react'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
-import { toast } from 'react-hot-toast'
-import { PlusCircle, Trash } from 'lucide-react'
-import { Billboard, Category } from '@prisma/client'
-import { useParams, useRouter } from 'next/navigation'
+import * as z from "zod";
+import axios from "axios";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
+import { PlusCircle, Trash } from "lucide-react";
+import { Billboard, Category } from "@prisma/client";
+import { useParams, useRouter } from "next/navigation";
 
-import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button'
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -19,94 +19,94 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from '@/components/ui/form'
-import { Separator } from '@/components/ui/separator'
-import { Heading } from '@/components/ui/heading'
-import { AlertModal } from '@/components/modals/alert-modal'
+} from "@/components/ui/form";
+import { Separator } from "@/components/ui/separator";
+import { Heading } from "@/components/ui/heading";
+import { AlertModal } from "@/components/modals/alert-modal";
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
-} from '@/components/ui/select'
-import { SelectValue } from '@radix-ui/react-select'
-import Image from 'next/image'
+} from "@/components/ui/select";
+import { SelectValue } from "@radix-ui/react-select";
+import Image from "next/image";
 
 const formSchema = z.object({
   name: z.string().min(1),
   billboardId: z.string().min(1),
-})
+});
 
-type CategoryFormValues = z.infer<typeof formSchema>
+type CategoryFormValues = z.infer<typeof formSchema>;
 
 interface CategoryFormProps {
-  initialData: Category | null
-  billboards: Billboard[]
+  initialData: Category | null;
+  billboards: Billboard[];
 }
 
 export const CategoryForm: React.FC<CategoryFormProps> = ({
   initialData,
   billboards,
 }) => {
-  const params = useParams()
-  const router = useRouter()
+  const params = useParams();
+  const router = useRouter();
 
-  const [open, setOpen] = useState(false)
-  const [loading, setLoading] = useState(false)
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const title = initialData ? 'Edit category' : 'Create category'
-  const description = initialData ? 'Edit a category.' : 'Add a new category'
-  const toastMessage = initialData ? 'category updated.' : 'category created.'
-  const action = initialData ? 'Save changes' : 'Create'
-  const buttonText = initialData ? 'Update Image' : 'Upload an Image'
+  const title = initialData ? "Edit category" : "Create category";
+  const description = initialData ? "Edit a category." : "Add a new category";
+  const toastMessage = initialData ? "category updated." : "category created.";
+  const action = initialData ? "Save changes" : "Create";
+  const buttonText = initialData ? "Update Image" : "Upload an Image";
 
   const form = useForm<CategoryFormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: initialData || {
-      name: '',
-      billboardId: '',
+      name: "",
+      billboardId: "",
     },
-  })
+  });
 
   const onSubmit = async (data: CategoryFormValues) => {
     try {
-      setLoading(true)
+      setLoading(true);
       if (initialData) {
         await axios.patch(
-          `/api/${params.storeId}/categories/${params.categoeyId}`,
+          `/api/${params.storeId}/categories/${params.categoryId}`,
           data
-        )
+        );
       } else {
-        await axios.post(`/api/${params.storeId}/categories`, data)
+        await axios.post(`/api/${params.storeId}/categories`, data);
       }
-      router.refresh()
-      router.push(`/${params.storeId}/categories`)
-      toast.success(toastMessage)
+      router.refresh();
+      router.push(`/${params.storeId}/categories`);
+      toast.success(toastMessage);
     } catch (error: any) {
-      toast.error('Something went wrong.')
+      toast.error("Something went wrong.");
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }
+  };
 
   const onDelete = async () => {
     try {
-      setLoading(true)
+      setLoading(true);
       await axios.delete(
         `/api/${params.storeId}/categories/${params.categoryId}`
-      )
-      router.refresh()
-      router.push(`/${params.storeId}/categories`)
-      toast.success('Category deleted.')
+      );
+      router.refresh();
+      router.push(`/${params.storeId}/categories`);
+      toast.success("Category deleted.");
     } catch (error: any) {
       toast.error(
-        'Make sure you removed all products using this category first.'
-      )
+        "Make sure you removed all products using this category first."
+      );
     } finally {
-      setLoading(false)
-      setOpen(false)
+      setLoading(false);
+      setOpen(false);
     }
-  }
+  };
 
   return (
     <>
@@ -116,16 +116,16 @@ export const CategoryForm: React.FC<CategoryFormProps> = ({
         onConfirm={onDelete}
         loading={loading}
       />
-      <div className='flex items-center justify-between'>
+      <div className="flex items-center justify-between">
         <Heading title={title} description={description} />
         {initialData && (
           <Button
             disabled={loading}
-            variant='destructive'
-            size='sm'
+            variant="destructive"
+            size="sm"
             onClick={() => setOpen(true)}
           >
-            <Trash className='w-4 h-4' />
+            <Trash className="w-4 h-4" />
           </Button>
         )}
       </div>
@@ -133,19 +133,19 @@ export const CategoryForm: React.FC<CategoryFormProps> = ({
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
-          className='w-full space-y-8'
+          className="w-full space-y-8"
         >
-          <div className='gap-8 md:grid md:grid-cols-3'>
+          <div className="gap-8 md:grid md:grid-cols-3">
             <FormField
               control={form.control}
-              name='name'
+              name="name"
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Name</FormLabel>
                   <FormControl>
                     <Input
                       disabled={loading}
-                      placeholder='Category Name'
+                      placeholder="Category Name"
                       {...field}
                     />
                   </FormControl>
@@ -155,7 +155,7 @@ export const CategoryForm: React.FC<CategoryFormProps> = ({
             />
             <FormField
               control={form.control}
-              name='billboardId'
+              name="billboardId"
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Billboard</FormLabel>
@@ -170,20 +170,20 @@ export const CategoryForm: React.FC<CategoryFormProps> = ({
                         <SelectTrigger>
                           <SelectValue
                             defaultValue={field.value}
-                            placeholder='Select a Billboard'
+                            placeholder="Select a Billboard"
                           />
                         </SelectTrigger>
                       </FormControl>
-                      <SelectContent className=''>
+                      <SelectContent className="">
                         {billboards.map((billboard) => (
                           <SelectItem key={billboard.id} value={billboard.id}>
-                            <div className='flex gap-3 items-center'>
-                              <div className='relative h-6 w-8 '>
+                            <div className="flex items-center gap-3">
+                              <div className="relative w-8 h-6 ">
                                 <Image
                                   src={billboard.imageUrl}
                                   fill
-                                  className='object-cover rounded'
-                                  alt='Billboards'
+                                  className="object-cover rounded"
+                                  alt="Billboards"
                                 />
                               </div>
                               <p>{billboard.label}</p>
@@ -198,11 +198,11 @@ export const CategoryForm: React.FC<CategoryFormProps> = ({
               )}
             />
           </div>
-          <Button disabled={loading} className='ml-auto' type='submit'>
+          <Button disabled={loading} className="ml-auto" type="submit">
             {action}
           </Button>
         </form>
       </Form>
     </>
-  )
-}
+  );
+};
