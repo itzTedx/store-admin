32bdb05e8fda064f2d17e22212312754667a06bb
melwin-tedd <musictrendiez@gmail.com>
2023-10-28 08:53:55 +0400

    stripe integration

--- app/(dashboard)/[storeId]/(routes)/orders/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/page.tsx
@@ -1,53 +1,53 @@
-import { format } from "date-fns";
+import { format } from 'date-fns'
 
-import prismadb from "@/lib/prismadb";
-import OrderClient from "./components/order-client";
-import { OrderColumn } from "./components/columns";
-import { formatter } from "@/lib/utils";
-import { Metadata } from "next";
+import prismadb from '@/lib/prismadb'
+import OrderClient from './components/order-client'
+import { OrderColumn } from './components/columns'
+import { formatter } from '@/lib/utils'
+import { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: "Orders",
-};
+  title: 'Orders',
+}
 
 const OrdersPage = async ({ params }: { params: { storeId: string } }) => {
   const orders = await prismadb.order.findMany({
     where: { storeId: params.storeId },
     include: {
-      orderItem: {
+      orderItems: {
         include: {
           product: true,
         },
       },
     },
     orderBy: {
-      createdAt: "desc",
+      createdAt: 'desc',
     },
-  });
+  })
 
   const formattedOrders: OrderColumn[] = orders.map((item) => ({
     id: item.id,
     phone: item.phone,
     address: item.address,
-    products: item.orderItem
+    products: item.orderItems
       .map((orderItem) => orderItem.product.name)
-      .join(", "),
+      .join(', '),
     totalPrice: formatter.format(
-      item.orderItem.reduce((total, item) => {
-        return total + Number(item.product.price);
+      item.orderItems.reduce((total, item) => {
+        return total + Number(item.product.price)
       }, 0)
     ),
     isPaid: item.isPaid,
-    createdAt: format(item.createdAt, "MMMM do, yyyy"),
-  }));
+    createdAt: format(item.createdAt, 'MMMM do, yyyy'),
+  }))
 
   return (
-    <div className="flex-col">
-      <div className="flex-1 p-8 pt-6 space-y-4 ">
+    <div className='flex-col'>
+      <div className='flex-1 p-8 pt-6 space-y-4 '>
         <OrderClient data={formattedOrders} />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default OrdersPage;
+export default OrdersPage
