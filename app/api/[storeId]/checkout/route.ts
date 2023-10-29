b1d4ded2179844bb1bcb74fe80c3f11ed2de432d
melwin-tedd <musictrendiez@gmail.com>
2023-10-29 21:15:26 +0400

    middleware cors try

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -4,15 +4,15 @@ import { NextResponse } from "next/server"
 import { stripe } from "@/lib/stripe"
 import prismadb from "@/lib/prismadb"
 
-// const corsHeaders = {
-//   "Access-Control-Allow-Origin": "*",
-//   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-//   "Access-Control-Allow-Headers": "Content-Type, Authorization",
-// }
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+}
 
-// export async function OPTIONS() {
-//   return NextResponse.json({}, { headers: corsHeaders })
-// }
+export async function OPTIONS() {
+  return NextResponse.json({}, { headers: corsHeaders })
+}
 
 export async function POST(
   req: Request,
@@ -78,9 +78,9 @@ export async function POST(
   })
 
   return NextResponse.json(
-    { url: session.url }
-    // {
-    //   headers: corsHeaders,
-    // }
+    { url: session.url },
+    {
+      headers: corsHeaders,
+    }
   )
 }
