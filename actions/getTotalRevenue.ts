45c63420708183ff9f7ada2cb1e727bc331ee5c1
melwin-tedd <musictrendiez@gmail.com>
2023-11-03 21:00:59 +0400

    price bug fix

--- actions/getTotalRevenue.ts
+++ actions/getTotalRevenue.ts
@@ -1,4 +1,4 @@
-import prismadb from '@/lib/prismadb'
+import prismadb from "@/lib/prismadb"
 
 export const getTotalRevenue = async (storeId: string) => {
   const paidOrders = await prismadb.order.findMany({
@@ -17,7 +17,7 @@ export const getTotalRevenue = async (storeId: string) => {
 
   const totalRevenue = paidOrders.reduce((total, order) => {
     const orderTotal = order.orderItems.reduce((orderSum, item) => {
-      return orderSum + item.product.price.toNumber()
+      return orderSum + item.product.discountPrice.toNumber()
     }, 0)
 
     return total + orderTotal
