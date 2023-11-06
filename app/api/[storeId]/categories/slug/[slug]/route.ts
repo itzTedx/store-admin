188aebefcf868481d24ad7d31236ec40e6eaeb24
itzTedx <zomboi.offi@gmail.com>
2023-11-06 19:24:22 +0400

    route

--- app/api/[storeId]/categories/slug/[slug]/route.ts
+++ app/api/[storeId]/categories/slug/[slug]/route.ts
@@ -1,24 +1,24 @@
-import prismadb from "@/lib/prismadb"
-import { NextResponse } from "next/server"
+import prismadb from "@/lib/prismadb";
+import { NextResponse } from "next/server";
 
 export async function GET(
   req: Request,
   { params }: { params: { slug: string; id: string } }
 ) {
   try {
-    // if (!params.slug) {
-    //   return new NextResponse("Slug Required", { status: 400 })
-    // }
+    if (!params.slug) {
+      return new NextResponse("Slug Required", { status: 400 });
+    }
 
     const category = await prismadb.category.findUnique({
       where: {
         slug: params.slug,
       },
       include: { billboard: true, subcategory: true },
-    })
-    return NextResponse.json(category)
+    });
+    return NextResponse.json(category);
   } catch (error) {
-    console.log("[CATEGORY_GET]", error)
-    return new NextResponse("Internal Error", { status: 500 })
+    console.log("[CATEGORY_GET]", error);
+    return new NextResponse("Internal Error", { status: 500 });
   }
 }
