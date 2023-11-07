e7dfd43f0f4c078778c9699850e372b654bfc92a
itzTedx <zomboi.offi@gmail.com>
2023-11-07 14:25:42 +0400

    Subcategory Route

---
+++ app/api/[storeId]/categories/subcategory/[subcategorySlug]/route.ts
@@ -0,0 +1,24 @@
+import prismadb from "@/lib/prismadb";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { subcategorySlug: string; id: string } }
+) {
+  try {
+    if (!params.subcategorySlug) {
+      return new NextResponse("Slug Required", { status: 400 });
+    }
+
+    const category = await prismadb.subcategory.findUnique({
+      where: {
+        slug: params.subcategorySlug,
+      },
+      include: { products: true },
+    });
+    return NextResponse.json(category);
+  } catch (error) {
+    console.log("[SUBCATEGORY_GET]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
